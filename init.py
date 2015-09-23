e3cf187a4ec3b5b7dca368a6b297d9921290132a
steefdw <beatniak@gmail.com>
2015-09-23 21:49:57 +0200

    Some testing for adding permanent weekdays schedules

--- init.py
+++ init.py
@@ -9,8 +9,8 @@ Created on Sat Sep 19 14:59:25 2015
 from api.base import Init
 import time
 
-Hue = Init(True)
-print Hue.lights.getLights()
+Hue = Init(False)
+Hue.lights.getLights()
 
 while False:
     r = Hue.lights.toggle()
@@ -19,20 +19,22 @@ while False:
 
 #current = time.time()
 #newtime = time.strftime("%Y-%m-%dT%H:%M:%S",time.localtime(current+20))
-newtime = 'PT00:00:05'
+newtime = 'W124/T08:25:00'
+#newtime = 'PT00:00:03'
 payload = {
     'on':False,
-    "sat":255,
-    "bri":255,
-    "hue":'0'
+    'sat':255,
+    'bri':255,
+    'hue':33210
 }
 
-print Hue.schedule.add(newtime, payload)
-print Hue.schedule.get()
+Hue.schedule.add(newtime, payload, True)
+Hue.schedule.get()
 
-#todo
+#todo '''
 ''' 
      wobble(color, lo, hi, interval) - this changes bightness between lo,hi with interval int and color x
+       if color is a list, you can set a color for each lamp
      color(name) - make a hue value (0,65535) from a string. Defaults to white
      toggle(color, fadeTime) - turns lights on/off
      class Color(colorname,brightness,staturation) - create the payload with ease: color(...).payload()
